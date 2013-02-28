69e745cae576b950ba1fd14b589fbd0f2c829ddb
Mathew Daniel <mathew.daniel@nexwave.ca>
2013-02-27 23:48:39 -0500

    Updates for twrp.
    Change-Id: Iad396f8342bfa515299a638402d3fea9c9254447

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,3 +10,13 @@ TARGET_OTA_ASSERT_DEVICE := p930
 TARGET_KERNEL_CONFIG := cyanogenmod_p930_defconfig
 # Keep this as a fallback
 TARGET_PREBUILT_KERNEL := device/lge/p930/kernel
+
+# TWRP flags
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_NO_USB_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
